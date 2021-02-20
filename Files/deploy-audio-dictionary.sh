c1934ae8d4da07b2b0c8d02ee8df3ba9d17e7ca2
Alex Isayenko <Alex.Isayenko@extraenergy.com.cy>
2021-02-21 01:19:50 +0200

    deployment script. introduce variables for directories.

--- Files/deploy-audio-dictionary.sh
+++ Files/deploy-audio-dictionary.sh
@@ -5,6 +5,9 @@ CONFIGURATION=Release
 RUNTIME="ubuntu.20.04-x64" #"debian.10-x64"
 FRAMEWORK=net5.0
 
+LOCAL_PROJECT_DIR=/mnt/c/Alex/AudioDictionary/
+REMOTE_BIN_DIR=/opt/audio-dictionary/
+
 ### Functions
 show-message(){
   echo -e "* \e[34m$1\e[39m *"
@@ -19,31 +22,34 @@ copy-full-package(){
   scp audio-dictionary.tar.xz alex-ocean:/tmp/
 
   show-message "clean up remote directory and unarchive binaries"
-  ssh alex-ocean rm -r /opt/audio-dictionary/*
-  ssh alex-ocean tar -xJf /tmp/audio-dictionary.tar.xz -C /opt/audio-dictionary/
-  ssh alex-ocean chmod a+x /opt/audio-dictionary/AudioDictionary
+  ssh alex-ocean rm -r $REMOTE_BIN_DIR/*
+  ssh alex-ocean tar -xJf /tmp/audio-dictionary.tar.xz -C $REMOTE_BIN_DIR/
+  ssh alex-ocean chmod a+x $REMOTE_BIN_DIR/AudioDictionary
 }
 
 copy-minimal(){
   show-message "copy files"
   cd ./bin/$CONFIGURATION/$FRAMEWORK/$RUNTIME/publish/
-  scp AudioDictionary* alex-ocean:/opt/audio-dictionary/
-  ssh alex-ocean chmod a+x /opt/audio-dictionary/AudioDictionary*
+
+  scp AudioDictionary alex-ocean:$REMOTE_BIN_DIR/
+  scp AudioDictionary.dll alex-ocean:$REMOTE_BIN_DIR/
+
+  ssh alex-ocean chmod a+x $REMOTE_BIN_DIR/AudioDictionary*
 }
 
 
 ### Main Script
 show-message "publishing"
-cd /mnt/c/Alex/AudioDictionary/
+cd $LOCAL_PROJECT_DIR
 show-message "dotnet publish -c $CONFIGURATION -f $FRAMEWORK -r $RUNTIME --self-contained"
 dotnet publish -c $CONFIGURATION -f $FRAMEWORK -r $RUNTIME --self-contained
 
 # copy-full-package
 copy-minimal
 
-scp /mnt/c/Alex/AudioDictionary/Files/audio-dictionary-generator.php alex-ocean:/var/www/html/
+scp $LOCAL_PROJECT_DIR/Files/audio-dictionary-generator.php alex-ocean:/var/www/html/
 
 ssh alex-ocean "sed -i \"s/datetimeplaceholder/`date +'%H:%M %d.%m.%Y'`/g\" /var/www/html/audio-dictionary-generator.php"
-ssh alex-ocean cp /opt/audio-dictionary/Files/silence-0.5s.mp3 /srv/audio-dictionary
+# ssh alex-ocean cp $REMOTE_BIN_DIR/Files/silence-0.5s.mp3 /srv/audio-dictionary
 
 show-message "done"
